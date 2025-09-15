dec7a9922d5d097f903b7adfbeaabdc2cfc39353
MariamE04 <elmirmariam16@gmail.com>
2025-09-15 11:49:44 +0200

    opsætning af projektet

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+import app.config.HibernateConfig;
+import jakarta.persistence.EntityManagerFactory;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+        System.out.println("test");
+
+    }
+}
