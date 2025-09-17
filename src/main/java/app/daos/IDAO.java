ce3dc0c57d42624b25b79f51ccf50faef2285d2a
MariamE04 <elmirmariam16@gmail.com>
2025-09-17 09:54:59 +0200

    interface tilføjet

---
+++ src/main/java/app/daos/IDAO.java
@@ -0,0 +1,12 @@
+package app.daos;
+
+import java.util.List;
+
+public interface IDAO<T,I> {
+
+    T creat(T t);
+    T getById(I i);
+    T update(T t);
+    List<T> getAll();
+    boolean delete(I i);
+}
